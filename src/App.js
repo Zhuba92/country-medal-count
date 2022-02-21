66a8b9a153e250c7f2a7d2626f55f798bc36bfc7
zachary <zhuba@my.wctc.edu>
2022-02-21 17:39:09 -0600

    Save increments to medal counts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react';
+import NewCountry from './components/NewCountry';
+import axios from 'axios';
 import './App.css';
 import Country from './components/Country';
-import NewCountry from './components/NewCountry';
+
 
 const App = () => {
 
   const [countries, setCountries] = useState([]);
+  const apiEndpoint = "https://olympic-medals-api-backend.azurewebsites.net/api/country"
 
   const medals = useRef([
     {id: 1, name: 'gold'},
@@ -20,37 +23,85 @@ const App = () => {
     return goldMedals + silverMedals + bronzeMedals;
   }
 
-  const handleAddCountry = (name) => {
-    const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-    setCountries([...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
+  // const handleAddCountry = (name) => {
+  //   const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
+  //   setCountries([...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
+  // }
+
+  const handleAddCountry = async (name) => {
+    const { data: post } = await axios.post(apiEndpoint, { name: name });
+    setCountries(countries.concat(post));
   }
 
-  const handleDeleteCountry = (countryID) => {
-    setCountries([...countries ].filter(c => c.id !== countryID));
+  // const handleDeleteCountry = (countryID) => {
+  //   setCountries([...countries ].filter(c => c.id !== countryID));
+  // }
+
+  const handleDeleteCountry = async (countryID) => {
+    const originalCountries = countries;
+    setCountries(countries.filter(c => c.id !== countryID));
+    try {
+      await axios.delete(`${apiEndpoint}/${countryID}`);
+    } catch(ex) {
+      if (ex.response && ex.response.status === 404) {
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while deleting a country');
+        setCountries(originalCountries);
+      }
+    }
   }
 
-  const handleIncrement = (countryID, medalName) => {
-    //this.setState({gold: this.state.gold + 1});
-    const countryIndex = countries.findIndex(c => c.id === countryID);
-    const countriesMutable = [...countries];
-    countriesMutable[countryIndex][medalName] += 1;
-    setCountries([...countries]);
-  }
+  // const handleIncrement = (countryID, medalName) => {
+  //   //this.setState({gold: this.state.gold + 1});
+  //   const countryIndex = countries.findIndex(c => c.id === countryID);
+  //   const countriesMutable = [...countries];
+  //   countriesMutable[countryIndex][medalName] += 1;
+  //   setCountries([...countries]);
+  // }
 
-  const handleDecrement = (countryID, medalName) => {
-    const countryIndex = countries.findIndex(c => c.id === countryID);
-    const countriesMutable = [...countries];
-    countriesMutable[countryIndex][medalName] -= 1;
-    setCountries([...countries]);
+  // const handleDecrement = (countryID, medalName) => {
+  //   const countryIndex = countries.findIndex(c => c.id === countryID);
+  //   const countriesMutable = [...countries];
+  //   countriesMutable[countryIndex][medalName] -= 1;
+  //   setCountries([...countries]);
+  // }
+
+  const handleIncrement = (countryId, medalName) => handleUpdate(countryId, medalName, 1);
+  const handleDecrement = (countryId, medalName) =>  handleUpdate(countryId, medalName, -1)
+  const handleUpdate = async (countryId, medalName, factor) => {
+    const originalCountries = countries;
+    const idx = countries.findIndex(c => c.id === countryId);
+    const mutableCountries = [...countries ];
+    mutableCountries[idx][medalName] += (1 * factor);
+    setCountries(mutableCountries);
+    const jsonPatch = [{ op: "replace", path: medalName, value: mutableCountries[idx][medalName] }];
+    console.log(`json patch for id: ${countryId}: ${JSON.stringify(jsonPatch)}`);
+
+    try {
+      await axios.patch(`${apiEndpoint}/${countryId}`, jsonPatch);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404) {
+        // country already deleted
+        console.log("The record does not exist - it may have already been deleted");
+      } else { 
+        alert('An error occurred while updating');
+        setCountries(originalCountries);
+      }
+    }
   }
 
   useEffect(() => {
-    let mutableCountries = [
-      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 6 },
-      { id: 2, name: 'China', gold: 3, silver: 5, bronze: 8 },
-      { id: 3, name: 'Germany', gold: 0, silver: 4, bronze: 5 },
-    ]
-    setCountries(mutableCountries);
+    // let mutableCountries = [
+    //   { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 6 },
+    //   { id: 2, name: 'China', gold: 3, silver: 5, bronze: 8 },
+    //   { id: 3, name: 'Germany', gold: 0, silver: 4, bronze: 5 },
+    // ]
+    async function fetchData() {
+      const { data: fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchData();
   }, [])
 
 
